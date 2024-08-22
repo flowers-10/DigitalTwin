7943f819151e0b0e7e947466c2c68a88a6bc4bea
Michela <mgaligigi@163.com>
2024-08-22 09:33:33 +0800

    refactor:use three-auto v0.1.8

--- src/hook/useThree.ts
+++ src/hook/useThree.ts
@@ -1,12 +1,12 @@
 import * as THREE from "three";
 import gsap from "gsap";
 import CustomShaderMaterial from "three-custom-shader-material/vanilla";
-import { ThreeInstance } from "three-auto";
+import * as AUTO from "three-auto";
 import buildingOtherVertex from "@shaders/buildingOther/vertex.glsl";
 import buildingOtherFragment from "@shaders/buildingOther/fragment.glsl";
 
 // todo:  custom config
-const sources = [
+const sources:AUTO.SourcesItems[] = [
   {
     name: "buildingTransparent",
     type: "GLTF",
@@ -69,35 +69,36 @@ const createGsapAnimation = (
     yoyoEase: true,
   });
 };
-const geometry = new THREE.BufferGeometry(); //声明一个空几何体对象
-// wall Vertex
-const posArr = [
-  -125, -20, 0, 210, -40, 0, 210, -40, 40, -125, -20, 0, 210, -40, 40, -125,
-  -20, 40, 210, -40, 0, 210, 60, 0, 210, 60, 40, 210, -40, 0, 210, 60, 40, 210,
-  -40, 40, 210, 60, 0, 180, 165, 0, 180, 165, 40, 210, 60, 0, 180, 165, 40, 210,
-  60, 40, 180, 165, 0, -130, 134, 0, -130, 134, 40, 180, 165, 0, -130, 134, 40,
-  180, 165, 40, -130, 134, 0, -125, -20, 0, -125, -20, 40, -130, 134, 0, -125,
-  -20, 40, -130, 134, 40,
-];
-// 设置几何体attributes属性的位置position属性
-geometry.attributes.position = new THREE.BufferAttribute(
-  new Float32Array(posArr),
-  3
-);
-geometry.computeVertexNormals();
+const createWall = () => {
+  const geometry = new THREE.BufferGeometry(); //声明一个空几何体对象
+  // wall Vertex
+  const posArr = [
+    -125, -20, 0, 210, -40, 0, 210, -40, 40, -125, -20, 0, 210, -40, 40, -125,
+    -20, 40, 210, -40, 0, 210, 60, 0, 210, 60, 40, 210, -40, 0, 210, 60, 40,
+    210, -40, 40, 210, 60, 0, 180, 165, 0, 180, 165, 40, 210, 60, 0, 180, 165,
+    40, 210, 60, 40, 180, 165, 0, -130, 134, 0, -130, 134, 40, 180, 165, 0,
+    -130, 134, 40, 180, 165, 40, -130, 134, 0, -125, -20, 0, -125, -20, 40,
+    -130, 134, 0, -125, -20, 40, -130, 134, 40,
+  ];
+  // 设置几何体attributes属性的位置position属性
+  geometry.attributes.position = new THREE.BufferAttribute(
+    new Float32Array(posArr),
+    3
+  );
+  geometry.computeVertexNormals();
 
-const material = new CustomShaderMaterial({
-  baseMaterial: THREE.ShaderMaterial,
-  uniforms: {
-    iTime: { value: 0 },
-  },
-  vertexShader: `
+  const material = new CustomShaderMaterial({
+    baseMaterial: THREE.ShaderMaterial,
+    uniforms: {
+      iTime: { value: 0 },
+    },
+    vertexShader: `
       varying vec3 vPosition;
       void main(){   
         csm_PositionRaw = projectionMatrix * modelViewMatrix * vec4(csm_Position, 1.0);
         vPosition = csm_Position;
     }`,
-  fragmentShader: `
+    fragmentShader: `
       uniform float iTime;
       varying vec3 vPosition;
 
@@ -108,12 +109,14 @@ const material = new CustomShaderMaterial({
         csm_FragColor = vec4( .2,1.,1.,alpha  );
       }  
     `,
-  side: THREE.DoubleSide,
-  transparent: true,
-});
-const mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
-mesh.name = "wall";
-mesh.rotateX(-Math.PI / 2);
+    side: THREE.DoubleSide,
+    transparent: true,
+  });
+  const mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+  mesh.name = "wall";
+  mesh.rotateX(-Math.PI / 2);
+  return mesh;
+};
 
 let buildingOtherMaterial: THREE.ShaderMaterial;
 const buildingOtherUniforms = {
@@ -128,7 +131,7 @@ const buildingOtherUniforms = {
   },
 };
 
-const setCityLineMaterial = (object: any, instance: ThreeInstance) => {
+const setCityLineMaterial = (object: any, instance: AUTO.ThreeAuto) => {
   const edges = new THREE.EdgesGeometry(object.geometry, 1);
   //设置模型的材质
   const lineMaterial = new THREE.LineBasicMaterial({
@@ -146,41 +149,43 @@ const setCityLineMaterial = (object: any, instance: ThreeInstance) => {
 };
 
 const useThree = (canvas: HTMLCanvasElement) => {
-  const instance: ThreeInstance = new ThreeInstance(canvas);
-  instance.camera.instance.fov = 75
-  instance.camera.instance.near = 20
-  instance.camera.instance.position.set(-200,400,-200)
+  const instance: AUTO.ThreeAuto = new AUTO.ThreeAuto(canvas);
+  const resources = new AUTO.Resources(sources)
+  instance.camera.instance.fov = 75;
+  instance.camera.instance.near = 20;
+  instance.camera.instance.position.set(-200, 400, -200);
   const initScene = new THREE.Group();
   const changScene = new THREE.Group();
   instance.scene.add(initScene);
   instance.scene.add(changScene);
-  instance.resources.on("ready", () => {
-    for (const key in instance.resources.items) {
-      const items = instance.resources.items[key];
-      if (key === "buildingOther") {
-        items.scene.children[0].children.forEach((item: any) => {
-          setCityLineMaterial(item, instance);
-        });
-      } else {
+  const wallMesh = createWall()
+  resources.on("ready", () => {
+    for (const key in resources.items) {
+      const items = resources.items[key];
+      // if (key === "buildingOther") {
+      //   items.scene.children[0].children.forEach((item: any) => {
+      //     setCityLineMaterial(item, instance);
+      //   });
+      // } else {
         items.show ? initScene.add(items.scene) : changScene.add(items.scene);
-      }
+      // }
     }
-    initScene.add(mesh);
+    // initScene.add(wallMesh);
     createGsapAnimation(
       instance.camera.instance.position,
       new THREE.Vector3(-20, 360, 166)
     );
   });
   instance.time.on("tick", () => {
-    material.uniforms.iTime.value = instance.time.elapsedTime;
-    buildingOtherUniforms.iTime.value = instance.time.elapsedTime;
-    if (
-      buildingOtherUniforms.height.value > buildingOtherUniforms.maxHeight.value
-    ) {
-      buildingOtherUniforms.height.value = 0;
-    } else {
-      buildingOtherUniforms.height.value += 0.3;
-    }
+    // wallMesh.material.uniforms.iTime.value = instance.time.elapsedTime;
+    // buildingOtherUniforms.iTime.value = instance.time.elapsedTime;
+    // if (
+    //   buildingOtherUniforms.height.value > buildingOtherUniforms.maxHeight.value
+    // ) {
+    //   buildingOtherUniforms.height.value = 0;
+    // } else {
+    //   buildingOtherUniforms.height.value += 0.3;
+    // }
   });
   instance.renderer.info();
   instance.sizes.info();
